89ed4d6ff363d625c4a2d7efa4d47403772fd538
Huw Fernie <HuwFernie@gmail.com>
2018-10-16 23:16:34 +0100

    think this is working with image upload now

--- client/src/components/camera.js
+++ client/src/components/camera.js
@@ -1,15 +1,76 @@
 import React from 'react';
 
-import Button from '../atoms/button';
-import Copy from '../atoms/copy';
+class AppIndex extends React.Component {
+  constructor(props) {
+    super(props);
 
-const AppIndex = () => {
-  return (
-    <div className="camera">
-      <Button text="Click" link="" />
-      <Copy copy=":Camera:" />
-    </div>
-  );
+    this.state = {
+      imageURL: '',
+    };
+
+    this.handleUploadImage = this.handleUploadImage.bind(this);
+    this.sendRequest = this.sendRequest.bind(this);
+  }
+
+  getBase64(file, cb) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        cb(reader.result)
+    };
+    reader.onerror = function (error) {
+        console.log('Error: ', error);
+    };
+  }
+
+  sendRequest(encodedString) {
+
+    const data = {
+      'name': this.fileName.value,
+      'base64': encodedString
+    };
+
+    fetch('http://localhost:3000/api/foodTrucks/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    }).then((response) => {
+      response.json().then((body) => {
+        this.setState({ imageURL: `${body.imageSRC}` });
+      });
+    });
+  }
+
+  handleUploadImage(ev) {
+    ev.preventDefault();
+
+    let fileBase64 = '';
+    this.getBase64(this.uploadInput.files[0], (result) => {
+         fileBase64 = result;
+         this.sendRequest(fileBase64);
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleUploadImage}>
+        <div>
+          <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+        </div>
+        <div>
+          <input ref={(ref) => { this.fileName = ref; }} type="text" placeholder="Enter the desired name of file" />
+        </div>
+        <br />
+        <div>
+          <button>Upload</button>
+        </div>
+        <img src={this.state.imageURL} alt="img" />
+      </form>
+    );
+  }
 }
 
 export default AppIndex;
--- models/truck.js
+++ models/truck.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-// const s3 = require('../lib/s3');
-// const configS3 = require('../config/s3');
+const s3 = require('../lib/s3');
+const configS3 = require('../config/s3');
 
 
 const foodTruckSchema = new mongoose.Schema({
-  name: { type: String, required: true }//,
+  name: { type: String, required: true },
   // category: { type: String, required: true },
   // location: {
   //   name: { type: String, required: true },
@@ -17,38 +17,38 @@ const foodTruckSchema = new mongoose.Schema({
   // },
   // website: { type: String, required: false },
   // details: { type: String, required: false },
-  // image: { type: String, required: true },
+  image: { type: String, required: true }//,
   // startTime: { type: Date, required: true }, //Start time, date set to /01/02/2003
   // finishTime: { type: Date, required: true }, //new Date(17, 7, 9, 8, 0, 0, 0),
   // date: { type: Date, required: true } // new Date(17, 7, 9, 17, 0, 0, 0)
 });
 
-// foodTruckSchema
-//   .path('image')
-//   .set(function getPreviousImage(image) {
-//     this._image = this.image;
-//     return image;
-//   });
+foodTruckSchema
+  .path('image')
+  .set(function getPreviousImage(image) {
+    this._image = this.image;
+    return image;
+  });
 
-// foodTruckSchema
-//   .virtual('imageSRC')
-//   .get(function getImageSRC() {
-//     if(!this.image) return null;
-//     if(this.image.match(/^http/)) return this.image;
-//     return `https://s3-eu-west-1.amazonaws.com/${configS3.paramsBucket}/${this.image}`;
-//   });
+foodTruckSchema
+  .virtual('imageSRC')
+  .get(function getImageSRC() {
+    if(!this.image) return null;
+    if(this.image.match(/^http/)) return this.image;
+    return `https://s3-eu-west-1.amazonaws.com/${configS3.paramsBucket}/${this.image}`;
+  });
 
 
-// foodTruckSchema.pre('remove', function removeImage(next) {
-//   if(this.image) return s3.deleteObject({ Key: this.image }, next);
-//   next();
-// });
+foodTruckSchema.pre('remove', function removeImage(next) {
+  if(this.image) return s3.deleteObject({ Key: this.image }, next);
+  next();
+});
 
-// foodTruckSchema.pre('save', function checkPreviousImage(next) {
-//   if(this.isModified('image') && this._image) {
-//     return s3.deleteObject({ Key: this._image }, next);
-//   }
-//   next();
-// });
+foodTruckSchema.pre('save', function checkPreviousImage(next) {
+  if(this.isModified('image') && this._image) {
+    return s3.deleteObject({ Key: this._image }, next);
+  }
+  next();
+});
 
 module.exports = mongoose.model('Truck', foodTruckSchema);
