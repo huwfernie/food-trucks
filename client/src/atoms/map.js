01eeee530402257165bc618ff260444ce49b764e
Huw Fernie <HuwFernie@gmail.com>
2018-10-22 20:33:58 +0100

    first bit of the google map

--- client/src/atoms/map.js
+++ client/src/atoms/map.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps'
+
+const AirbnbMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultCenter={props.center}
+    defaultZoom={props.zoom} />
+));
 
 class Map extends Component {
- render() {
-    return (
-      <div className='map'>:MAP:</div>
-    )
+  constructor(props) {
+    super(props)
+
+    this.zoom = 7
+
+    this.state = {
+      lat: 50.0515918,
+      lng: 19.9357531
+    };
+  }
+
+  render() {
+    const {lat, lng} = this.state;
+
+    return(
+      <div style={{width: `750px`, height: `750px`}}>
+        <AirbnbMap
+          center={{
+            lat: lat,
+            lng: lng
+          }}
+          zoom={this.zoom}
+          containerElement={
+            <div style={{ height: `100%` }} />
+          }
+          mapElement={
+            <div style={{ height: `100%` }} />
+          }
+        />
+      </div>
+    );
   }
 }
+
 
 export default Map;
