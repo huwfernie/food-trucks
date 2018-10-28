1eb09dfda8113e6f3f99ec57b0fc042da1aed7f1
Huw Fernie <HuwFernie@gmail.com>
2018-10-28 18:03:21 +0000

    added a 10minute walk circle to the map

--- client/src/atoms/map.js
+++ client/src/atoms/map.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Circle } from 'react-google-maps';
 
 const AirbnbMap = withGoogleMap(props => (
   <GoogleMap
     defaultCenter={props.defaultCenter}
     center={props.center}
-    defaultZoom={props.zoom}
-  />
+    defaultZoom={props.defaultZoom}
+    zoom={props.zoom}
+  >
+    <Circle
+      defaultCenter={props.defaultCenter}
+      center={props.center}
+      defaultRadius={965}
+      radius={965}
+      options={
+        {strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35,}
+        } />
+  </GoogleMap>
 ));
 
 class Map extends Component {
   constructor(props) {
     super(props);
-
-    this.zoom = 7;
 
     this.state = {
       default: {
@@ -22,25 +34,28 @@ class Map extends Component {
       },
 
       lat: 0,
-      lng: 0
+      lng: 0,
+
+      zoo: 7
     };
   }
 
   componentDidMount() {
-    console.log(navigator.geolocation.getCurrentPosition);
-
+    // console.log(navigator.geolocation.getCurrentPosition);
     navigator.geolocation.getCurrentPosition(result => {
       console.log(result.coords.latitude);
       console.log(result.coords.longitude);
+      console.log('found your location');
       this.setState({
         lat: result.coords.latitude,
-        lng: result.coords.longitude
+        lng: result.coords.longitude,
+        zoo: 14
       });
     });
   }
 
   render() {
-    const { lat, lng } = this.state;
+    const { lat, lng, zoo } = this.state;
 
     return (
       <div style={{ width: `750px`, height: `750px` }}>
@@ -53,59 +68,34 @@ class Map extends Component {
             lat: lat,
             lng: lng
           }}
-          zoom={7}
+
+          defaultZoom = {7}
+          zoom = {zoo}
+
           containerElement={<div style={{ height: `100%` }} />}
           mapElement={<div style={{ height: `100%` }} />}
-        />
+        >
+          <Circle
+            defaultCircleCenter={{
+              lat: this.state.default.lat,
+              lng: this.state.default.lng
+            }}
+            circleCenter={{
+              lat: lat,
+              lng: lng
+            }}
+
+            defaultCircleRadius={{
+              rad: 600
+            }}
+            circleRadius={{
+              rad: 600
+            }}
+           />
+        </AirbnbMap>
       </div>
     );
   }
 }
 
 export default Map;
-//
-//
-// const AirbnbMap = withGoogleMap(props => (
-//   <GoogleMap
-//     defaultCenter={props.center}
-//     defaultZoom={props.zoom} />
-// ));
-//
-// class Map extends Component {
-//   constructor() {
-//     super();
-//     let personProfile = this.state = {
-//       name: "Bob",
-//       skill: "Art Designer",
-//       location: "LA"
-//     }
-//   }
-//
-//   componentDidMount() {
-//     setTimeout(() => {
-//       this.setState({name: "Frank"});
-//     }, 1000)
-//
-//     console.log(navigator.geolocation.getCurrentPosition);
-//
-//     navigator.geolocation.getCurrentPosition((result) => {
-//       console.log(result.coords.latitude);
-//       this.setState({name: result.coords.latitude});
-//     })
-//   }
-//
-//   render() {
-//     return (
-//       <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-//         <ul>
-//           <li> {this.state.name} </li>
-//           <li> {this.state.skill} </li>
-//           <li> {this.state.location} </li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-//
-//
-// export default Map;
